5bca7a1260661626084a20ee65f073c889ceb6a6
Parisa D <112294569+ParisaDK@users.noreply.github.com>
2025-09-08 22:10:34 -0500

    Update SinglyLinkedList.java

--- Linked_list/src/SinglyLinkedList.java
+++ Linked_list/src/SinglyLinkedList.java
@@ -1,18 +1,78 @@
-public class LinkedListADTImpl<T> implements ListADT<T> {
-    private final SinglyLinkedList<T> list = new SinglyLinkedList<>();
+package Linked_list.src;
 
-    @Override
-    public T access(int i) { return list.get(i); }
+import java.util.NoSuchElementException;
 
-    @Override
-    public void replace(T x, int i) { list.set(i, x); }
+public class SinglyLinkedList<T> {
 
-    @Override
-    public void insert(T x, int i) { list.insertAt(i, x); }
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+        Node(E d, Node<E> n) { data = d; next = n; }
+    }
 
-    @Override
-    public void remove(int i) { list.removeAt(i); }
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
 
-    @Override
-    public int size() { return list.size(); }
+    public int size() { return size; }
+    public boolean isEmpty() { return size == 0; }
+
+    private void checkIndexInclusive(int i) {
+        if (i < 0 || i >= size) throw new IndexOutOfBoundsException("Index: " + i);
+    }
+
+    private void checkIndexExclusive(int i) {
+        if (i < 0 || i > size) throw new IndexOutOfBoundsException("Index: " + i);
+    }
+
+    private Node<T> nodeAt(int i) {
+        Node<T> x = head;
+        for (int k = 0; k < i; k++) x = x.next;
+        return x;
+    }
+
+    // --- Access and Replace ---
+    public T get(int i) {
+        checkIndexInclusive(i);
+        return nodeAt(i).data;
+    }
+
+    public void set(int i, T x) {
+        checkIndexInclusive(i);
+        nodeAt(i).data = x;
+    }
+
+    // --- Insert ---
+    public void insertAt(int i, T x) {
+        checkIndexExclusive(i);
+        if (i == 0) {
+            head = new Node<>(x, head);
+            if (size == 0) tail = head;
+        } else if (i == size) {
+            Node<T> n = new Node<>(x, null);
+            if (isEmpty()) head = tail = n;
+            else { tail.next = n; tail = n; }
+        } else {
+            Node<T> prev = nodeAt(i - 1);
+            prev.next = new Node<>(x, prev.next);
+        }
+        size++;
+    }
+
+    // --- Remove ---
+    public T removeAt(int i) {
+        checkIndexInclusive(i);
+        if (i == 0) {
+            T val = head.data;
+            head = head.next;
+            if (--size == 0) tail = null;
+            return val;
+        }
+        Node<T> prev = nodeAt(i - 1);
+        Node<T> doomed = prev.next;
+        prev.next = doomed.next;
+        if (doomed == tail) tail = prev;
+        size--;
+        return doomed.data;
+    }
 }
